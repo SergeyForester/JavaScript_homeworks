d5c6d1d6ad9d463854720c41bf9fb3ad425d57f0
SergeyForester <40773987+SergeyForester@users.noreply.github.com>
2019-01-26 18:02:58 +0300

    Update step_1.js

--- hw_5/step_1/step_1.js
+++ hw_5/step_1/step_1.js
@@ -1,39 +1,38 @@
+'use strict';
+
 const chess = {
-  gameContainerEl: document.getElementById('game'),
-  render() {
- 
-    const cols = [0,'a','b', 'c', 'd', 'e', 'f', 'g','h',0];
+	gameContainerEl: document.getElementById('game'),
 
-		// Цикл для строк
-    for (let row = 0; row < 10; row++) {
-      const tr = document.createElement('tr');
-      this.gameContainerEl.appendChild(tr);
-			
-      // Добавляем ячейки в строку.
-      for (let col = 0; col < 10; col++) {
-        const td = document.createElement('td');
-        tr.appendChild(td);
+	renderMap() {
+	const cols = [0,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',0] 
+	const rows= [0, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-        if (row === 0 && cols[col] !== 0) {
-          td.innerHTML = cols[col];
-        } 
-	if (row === 9 && cols[col] !== 0) {
-          td.innerHTML = cols[col];
-        } 
+	for (let row = 0; row < rows.length; row++) {
+		const tr = document.createElement('tr');
+		this.gameContainerEl.appendChild(tr);
 
-        if (this.isCellIsBlack(row, col)) {
-          td.style.backgroundColor = 'grey';
-        }
-      }
-    }
-  },
+		for (let col = 0; col < cols.length; col++) {
+			const td = document.createElement('td');
+			tr.appendChild(td);
 
+			if (rows[row] === 0 && cols[col] !== 0) {
+				td.innerHTML = cols[col]
+			} else if (cols[col] === 0 && rows[row] !== 0) {
+				td.innerHTML = rows[row]
+			}
+			if (this.isCellIsBlack(row, col)) {
+				td.style.backgroundColor = 'green'
+			}
+		}
+	}
+	},
 
-  isCellIsBlack(rowNum, colNum) {
-
-   return (rowNum + colNum) % 2 === 0;
-  },
+	isCellIsBlack(rowNum, colNum) {
+		if (rowNum === 0 || rowNum === 9 || colNum === 0 || colNum === 9) {
+			return false;
+		}
+		return (rowNum + colNum) % 2 === 0;
+	}
 };
 
-// Запускаем метод render.
-chess.render();
+chess.renderMap();
