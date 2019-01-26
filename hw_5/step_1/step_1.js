3c56ccd4a9ceb6ad45d07f17ac80d61c1e5ab128
SergeyForester <40773987+SergeyForester@users.noreply.github.com>
2019-01-26 15:45:10 +0300

    Update step_1.js

--- hw_5/step_1/step_1.js
+++ hw_5/step_1/step_1.js
@@ -1,28 +1,24 @@
 const chess = {
   gameContainerEl: document.getElementById('game'),
   render() {
-  	// Названия колонок
+ 
     const cols = [0,'a','b', 'c', 'd', 'e', 'f', 'g','h',0];
 
 		// Цикл для строк
     for (let row = 0; row < 10; row++) {
-    	// Создаем и добавляем строку.
       const tr = document.createElement('tr');
       this.gameContainerEl.appendChild(tr);
 			
       // Добавляем ячейки в строку.
       for (let col = 0; col < 10; col++) {
-      	// Создаем и добавляем ячейку.
         const td = document.createElement('td');
         tr.appendChild(td);
 
-				// Если строка нулевая (первая по счету), значит выводим буквы в ней.
-        // Нули из массива с названиями колонок не выводим, они нам не нужны.
         if (row === 0 && cols[col] !== 0) {
           td.innerHTML = cols[col];
         } 
 
-        // Проверяем, надо ли покрасить ячейку, передаем строку и колонку.
+
         if (this.isCellIsBlack(row, col)) {
           td.style.backgroundColor = 'grey';
         }
@@ -30,10 +26,9 @@ const chess = {
     }
   },
 
-	// Метод определяет нужно ли покрасить ячейку.
-  // Просто для примера покрасим ячейки, строки которых делятся на 2 с остатком.
+
   isCellIsBlack(rowNum, colNum) {
-  	// Если будет остаток, то он даст true в условии, а если не будет, то 0 даст false в условии.
+
     let rows = rowNum % 2;
     let cols = colNum % 2;
     return rows, cols;
